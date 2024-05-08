eca4cce8921c23850512a7d838ada7ee05c3c6b4
s4llkohl <s4llkohl@uni-trier.de>
2024-05-08 08:04:15 +0200

    Änderung Main Branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,10 @@ public class Main {
 
         Car car1 = new Car("red", "BMW", 5);
         Car car2 = new Car("green", "Audi", 3);
-        Car car3 = new Car("blue", "VW", 3);
+        Car car4 = new Car("blue", "VW", 3);
 
-        Car car4 = new Car("yellow", "Renault", 4);
+        Car car3 = new Car("yellow", "Renault", 4);
+        // An dieser Stelle habe ich car3 und car4 vertauscht
 
         //An dieser Stelle befindet sich nun ein Array
         //Dieses Array wird mit einem Sortieralgorithmus sortiert
